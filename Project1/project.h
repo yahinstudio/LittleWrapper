8f19109f17637f4923eee86bc1a51b2dad8d7d59
asforest <asforest@qq.com>
2021-10-06 19:32:37 +0800

    chore(version): 1.1.0

--- Project1/project.h
+++ Project1/project.h
@@ -4,7 +4,7 @@
 #define ENABLED_ERROR_CHECK
 
 #define PROJECT_NAME "LittleWrapper"
-#define VERSION_TEXT "1.0.9"
+#define VERSION_TEXT "1.1.0"
 #define PROJ_VER PROJECT_NAME " v" VERSION_TEXT
 
 #define PATH_MAX 256
